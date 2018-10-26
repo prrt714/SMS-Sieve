58b67be03d12e10a74f364cf8b8da6b6dc542161
prrt714 <prrt714@gmail.com>
2018-10-26 14:40:56 +0300

    [x] migrate to gradle

---
+++ app/src/main/java/vnd/blueararat/smssieve/Fragment3.java
@@ -0,0 +1,35 @@
+package vnd.blueararat.smssieve;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment3 extends Fragment {
+
+    private TextView log;
+
+    public Fragment3() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment3, container, false);
+        String msglog = MainActivity.preferences
+                .getString(Receiver.KEY_LOG, "");
+        log = (TextView) rootView.findViewById(R.id.log);
+
+        if (!msglog.isEmpty()) {
+            log.setText(Html.fromHtml(msglog));
+        }
+        return rootView;
+    }
+
+    public void empty() {
+        log.setText(R.string.log_empty);
+    }
+}
---
+++ app/src/main/java/vnd/blueararat/smssieve/MainActivity.java
@@ -0,0 +1,419 @@
+package vnd.blueararat.smssieve;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Telephony;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int READ_SMS_PERMISSION_RESULT = 3;
+    static SharedPreferences preferences, filters, regex_filters, exceptions,
+            regex_exceptions;
+    static List<String> addresses;
+    static final String REGEX_PART1 = "regex(";
+    static final String EXC_PART1 = "excep(";
+    Fragment[] fr = new Fragment[3];
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            addresses = Collections.emptyList();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_SMS},
+                    READ_SMS_PERMISSION_RESULT);
+        } else {
+            addresses = getAddresses();
+        }
+
+        filters = getSharedPreferences(Receiver.FILTERS, MODE_PRIVATE);
+        regex_filters = getSharedPreferences(Receiver.REGEX_FILTERS,
+                MODE_PRIVATE);
+        exceptions = getSharedPreferences(Receiver.EXCEPTIONS, MODE_PRIVATE);
+        regex_exceptions = getSharedPreferences(Receiver.REGEX_EXCEPTIONS,
+                MODE_PRIVATE);
+        setContentView(R.layout.activity_main);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(
+                getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case READ_SMS_PERMISSION_RESULT: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    addresses = getAddresses();
+                    if (fr[1] != null)
+                        ((Fragment2) fr[1]).refresh();
+                }
+                break;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public List<String> getAddresses() {
+        Set<String> set = new HashSet<>();
+        // a.add("result1");
+        // a.add("result2");
+        // a.add("result3");
+
+        final String[] projection = new String[]{Telephony.Sms.ADDRESS};
+        Cursor cursor = getContentResolver().query(Telephony.Sms.Inbox.CONTENT_URI, projection, null, null, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int count = cursor.getCount();
+            for (int i = 0; i< count; i++) {
+                set.add(cursor.getString(0));
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+        String matches = preferences.getString(Receiver.KEY_MATCHES, "");
+        if (!matches.isEmpty()) {
+            String newadrs[] = matches.split(" ");
+            for (String s : newadrs) {
+                if (!s.isEmpty())
+                    set.add(s);
+            }
+        }
+
+        List<String> result = new ArrayList<>(set.size());
+        result.addAll(set);
+        Collections.sort(result, (a, b) -> b.toLowerCase().compareTo(a.toLowerCase()));
+        return result;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                final View view = inflater.inflate(R.layout.input_dialog, null,
+                        false);
+                ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_dropdown_item_1line, addresses);
+
+                final AutoCompleteTextView input = (AutoCompleteTextView) view
+                        .findViewById(R.id.input);
+                input.setAdapter(adapter2);
+                final CheckBox chb = (CheckBox) view.findViewById(R.id.chb_reg);
+                final CheckBox chb_exc = (CheckBox) view.findViewById(R.id.chb_exc);
+
+                new AlertDialog.Builder(this)
+                        .setMessage(R.string.input)
+                        .setView(view)
+                        .setPositiveButton(android.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int whichButton) {
+                                        String str = input.getText().toString();
+                                        String[] ss = str.split(";");
+                                        Editor et1 = exceptions.edit();
+                                        Editor et2 = regex_exceptions.edit();
+                                        Editor et3 = filters.edit();
+                                        Editor et4 = regex_filters.edit();
+                                        boolean isException = (chb_exc.isChecked() && !str
+                                                .contains(EXC_PART1));
+                                        boolean isRegex = (chb.isChecked() && !str
+                                                .contains(REGEX_PART1));
+                                        for (String s : ss) {
+                                            s = s.trim();
+                                            if (s.length() == 0)
+                                                continue;
+                                            boolean isexc = s.startsWith(EXC_PART1);
+                                            if (isexc)
+                                                s = s.substring(6,
+                                                        s.lastIndexOf(")"));
+                                            boolean isreg = s
+                                                    .startsWith(REGEX_PART1);
+                                            if (isreg)
+                                                s = s.substring(6,
+                                                        s.lastIndexOf(")"));
+                                            if (isRegex || isreg) {
+                                                if (isException || isexc) {
+                                                    if (!regex_exceptions
+                                                            .contains(s)) {
+                                                        if (isValid(s))
+                                                            et2.putInt(s, 0);
+                                                    }
+                                                } else {
+                                                    if (!regex_filters.contains(s)) {
+                                                        if (isValid(s))
+                                                            et4.putInt(s, 0);
+                                                    }
+                                                }
+                                            } else {
+                                                if (isException || isexc) {
+                                                    if (!exceptions.contains(s)) {
+                                                        et1.putInt(s, 0);
+                                                    }
+                                                } else {
+                                                    if (!filters.contains(s)) {
+                                                        if (isValid(s))
+                                                            et3.putInt(s, 0);
+                                                    }
+                                                }
+
+                                            }
+                                        }
+                                        et1.commit();
+                                        et2.commit();
+                                        et3.commit();
+                                        et4.commit();
+                                        if (fr[0] != null)
+                                            ((Fragment1) fr[0]).refresh(null);
+                                        if (fr[1] != null)
+                                            ((Fragment2) fr[1]).refresh();
+                                    }
+                                }).setNegativeButton(android.R.string.no, null)
+                        .show();
+
+                break;
+            case R.id.export:
+
+                Set<String> set = exceptions.getAll().keySet();
+                String s = "";
+                for (String el : set) {
+                    s += ";\n" + EXC_PART1 + el + ")";
+                }
+                set = regex_exceptions.getAll().keySet();
+                for (String el : set) {
+                    s += ";\n" + EXC_PART1 + REGEX_PART1 + el + "))";
+                }
+                set = filters.getAll().keySet();
+                for (String el : set) {
+                    s += ";\n" + el;
+                }
+                set = regex_filters.getAll().keySet();
+                for (String el : set) {
+                    s += ";\n" + REGEX_PART1 + el + ")";
+                }
+                if (s.startsWith(";\n"))
+                    s = s.substring(2);
+                Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                        .setType("plain/text").setText(s)
+                        // .setChooserTitle(R.string.share)
+                        .setSubject(getString(R.string.subject)).createChooserIntent();
+                startActivity(shareIntent);
+
+                break;
+            case R.id.help:
+                View view2 = LayoutInflater.from(this).inflate(
+                        R.layout.help_dialog, null, false);
+                ListView lv = (ListView) view2.findViewById(R.id.example_list);
+                TextView ht = (TextView) view2.findViewById(R.id.title);
+                ht.setText(getString(R.string.help_title,
+                        getString(android.R.string.yes)));
+                final String[] titles = getResources().getStringArray(
+                        R.array.examples_titles);
+                final String[] summaries = getResources().getStringArray(
+                        R.array.examples_explanations);
+                final boolean[] checked = {false, false, false, false};
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        R.layout.help_list_item2, titles) {
+
+                    @Override
+                    public View getView(int position, View convertView,
+                                        ViewGroup parent) {
+                        final int n = position;
+                        final boolean b4 = regex_filters.contains(titles[n]);
+                        View view;
+                        TextView title, summary;
+                        CheckBox ch;
+                        if (convertView == null) {
+                            view = LayoutInflater.from(getContext()).inflate(
+                                    R.layout.help_list_item2, parent, false);
+                        } else {
+                            view = convertView;
+                        }
+
+                        try {
+                            title = (TextView) view.findViewById(R.id.title3);
+                            summary = (TextView) view
+                                    .findViewById(R.id.explanation);
+                            ch = (CheckBox) view.findViewById(R.id.checkbox2);
+                        } catch (ClassCastException e) {
+                            throw new IllegalStateException(e.toString(), e);
+                        }
+                        title.setText(titles[position]);
+                        summary.setText(summaries[position]);
+                        ch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView,
+                                                         boolean isChecked) {
+                                checked[n] = isChecked;
+                            }
+                        });
+                        return view;
+                    }
+                };
+                lv.setAdapter(adapter);
+
+                new AlertDialog.Builder(this)
+                        .setView(view2)
+                        .setPositiveButton(android.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int whichButton) {
+                                        boolean changed = false;
+                                        Editor et = regex_filters.edit();
+                                        for (int i = 0; i < titles.length; i++) {
+                                            if (checked[i]) {
+                                                if (!regex_filters
+                                                        .contains(titles[i])) {
+                                                    et.putInt(titles[i], 0);
+                                                    changed = true;
+                                                }
+                                            }
+                                        }
+                                        if (changed) {
+                                            et.commit();
+                                            if (fr[0] != null)
+                                                ((Fragment1) fr[0]).refresh(null);
+                                            if (fr[1] != null)
+                                                ((Fragment2) fr[1]).refresh();
+                                        }
+                                    }
+                                }).setNegativeButton(android.R.string.no, null)
+                        .show();
+                break;
+            case R.id.clear_log:
+                new AlertDialog.Builder(this)
+                        // .setIcon(android.R.drawable.ic_dialog_alert)
+                        // .setTitle("")
+                        .setMessage(R.string.clear_all_sms)
+                        .setPositiveButton(android.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        Editor et = preferences.edit();
+                                        et.remove(Receiver.KEY_LOG);
+                                        et.commit();
+                                        if (fr[2] != null)
+                                            ((Fragment3) fr[2]).empty();
+                                    }
+
+                                }).setNegativeButton(android.R.string.no, null)
+                        .show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isValid(String s) {
+        Pattern p = null;
+        try {
+            p = Pattern.compile(s);
+        } catch (PatternSyntaxException e) {
+            Toast.makeText(
+                    this,
+                    getString(R.string.regex) + " \"" + s + "\" "
+                            + getString(R.string.wrong_pattern),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    fr[0] = new Fragment1();
+                    break;
+                case 1:
+                    fr[1] = new Fragment2();
+                    break;
+                case 2:
+                    fr[2] = new Fragment3();
+                    break;
+
+            }
+            return fr[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Locale l = Locale.getDefault();
+            switch (position) {
+                // case 0:
+                // return getString(R.string.title_section1);// .toUpperCase(l);
+                case 0:
+                    return getString(R.string.title_section1);// .toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2);// .toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3);// .toUpperCase(l);
+
+            }
+            return null;
+        }
+    }
+}
